8d2429e41413971d915fd61bcfb5756f45880411
Kevin Winter <kevin.n.winter@gmail.com>
2017-06-13 00:22:55 +0200

    part2

--- src/classify.py
+++ src/classify.py
@@ -2,6 +2,7 @@
 
 from preprocess import preprocess
 from parse_emails import *
+from helper_functions import Logger
 
 from time import time
 import numpy as np
@@ -10,53 +11,62 @@ from sklearn.externals import joblib
 
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.svm import SVC
+from sklearn.svm import SVC, LinearSVC
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.neural_network import MLPClassifier
-
-## CONFIGURATION
-# TARGETS (0: Authors, 1: Recipients, 2: Both)
-t = 0
-# CLASSIFIER (0: Naive Bayes, 1: Decision Tree, 2: SVM, 3: Random Forest, 4: Neural Network, 5: Gradient Boosting)
-c = 5
 
 try:
     with open("./data/authors.pkl", "rb") as authors_file, \
             open("./data/word_data.pkl", "rb") as words_file, \
             open("./data/recipients.pkl", "rb") as recipients_file, \
-            open("./data/classes.pkl", "rb") as classes:
+            open("./data/classes.pkl", "rb") as classes_file:
         authors = pickle.load(authors_file)
         recipients = pickle.load(recipients_file)
         word_data = pickle.load(words_file)
-        target_names = np.array(pickle.load(classes))
+        classes = np.array(pickle.load(classes_file))
 except Exception as e:
     print("Error in loading prepared data: {}\n Parsing data from database.".format(e))
-    word_data, authors, recipients, target_names = parse_mails()
+    word_data, authors, recipients, classes = parse_mails()
 
-targets = {0: authors, 1: recipients, 2: zip(authors, recipients)}.get(t)
-features_train, features_test, labels_train, labels_test = preprocess(word_data, targets)
 
+## CONFIGURATION
+t = 1
+c = 3
+
+target_options = {0: ("Authors", authors),
+                  1: ("Recipients", recipients),
+                  2: ("AuthorsAndRecipients)", zip(authors, recipients))}
 
 clfs = {0: ("Naive Bayes", MultinomialNB()),
         1: ("Decision Tree", DecisionTreeClassifier(min_samples_split=10)),
-        2: ("SVM", SVC(kernel="rbf", C=10500, verbose=True)),
-        3: ("Random Forest", RandomForestClassifier(verbose=3)),
-        4: ("Neural Network", MLPClassifier(hidden_layer_sizes=(100,), early_stopping=True, verbose=True)),
-        5: ("Gradient Boosting", GradientBoostingClassifier(verbose=1))}
-clf = clfs.get(c)[1]
-print("\n### CLASSIFICATION using {} ###".format(clfs.get(c)[0]))
+        2: ("Linear SVM", LinearSVC(verbose=1)),
+        3: ("RBF SVM", SVC(C=10500, kernel="rbf", verbose=True, decision_function_shape="ovr")),
+        4: ("Random Forest", RandomForestClassifier(verbose=3)),
+        5: ("Neural Network", MLPClassifier(hidden_layer_sizes=(100,), early_stopping=True, verbose=True)),
+        6: ("Gradient Boosting", GradientBoostingClassifier(verbose=1))}
 
-t0 = time()
-clf.fit(features_train, labels_train)
-print("Training time:", round((time()-t0)/60, 2), "m")
-joblib.dump(clf, "./data/{} Classifier.pkl".format(clfs.get(c)[0]))
+#for t in range(len(target_options)):
+for c in range(4,6):
+    title = "{} by {}".format(target_options.get(t)[0], clfs.get(c)[0])
+    sys.stdout = Logger(title)
+    targets = target_options.get(t)[1]
+    target_names = classes[np.unique(targets)]
+    features_train, features_test, labels_train, labels_test = preprocess(word_data, targets)
 
-t1 = time()
-prediction = clf.predict(features_test)
-print("Prediction time:", round((time()-t1)/60, 2), "m")
+    clf = clfs.get(c)[1]
+    print("\n### CLASSIFICATION using {} ###".format(clfs.get(c)[0]))
 
-print(classification_report(labels_test, prediction, target_names=target_names))
-print("Accuracy:", accuracy_score(prediction, labels_test))
+    t0 = time()
+    clf.fit(features_train, labels_train)
+    print("Training time:", round((time()-t0)/60, 2), "m")
+    joblib.dump(clf, "./data/{} Classifier.pkl".format(clfs.get(c)[0]))
 
-cnf_matrix = confusion_matrix(labels_test, prediction)
-plot_confusion_matrix(cnf_matrix, target_names[np.unique(targets)], normalize=True)
+    t1 = time()
+    prediction = clf.predict(features_test.toarray())
+    print("Prediction time:", round((time()-t1)/60, 2), "m")
+
+    print(classification_report(labels_test, prediction, target_names=target_names))
+    print("Accuracy:", accuracy_score(prediction, labels_test))
+
+    cnf_matrix = confusion_matrix(labels_test, prediction)
+    plot_confusion_matrix(cnf_matrix, target_names, title, show=False)
--- src/one_time_import/03_mark_redundant_paragraphs.py
+++ src/one_time_import/03_mark_redundant_paragraphs.py
@@ -1,49 +1,101 @@
 #!/usr/bin/env python3
+
 from DBHelper import DBHelper
 import numpy as np
 import time
 from datetime import timedelta, datetime
 
 db = DBHelper()
-db.execute("SET group_concat_max_len = 18446744073709547520")
-
 print("Fetching codes")
 results = db.execute("""
-    SELECT mailId, GROUP_CONCAT(pid SEPARATOR ',') as sha, count(*) c
-    from kddm2.mail_paragraphs
+    SELECT mailId, pid
+    from mail_paragraphs
     where deleted = 0
-    group by mailId
-    having c > 1
+    order by mailid, sortorder 
     """)
-totalemails = db.rowcount
 
-cnt, sum, dt, mails = 0, 0, 0, []
-base = [r[1] for r in results]
+ms = {} #all paragraphs per mail
+ps = {} #all mails per paragraph
+mailParas = {} #the string representation of all paragrpahs per mail
+for res in results:
+    if res[0] not in ms:
+        ms[res[0]] = []
 
-print("Finding redundancies in {} emails.".format(totalemails))
-t = time.perf_counter()
+    if res[0] not in mailParas:
+        mailParas[res[0]] = ""
+
+    if res[1] not in ps:
+        ps[res[1]] = set()
+
+    ms[res[0]].append(res[1])
+    ps[res[1]].add(res[0])
+    mailParas[res[0]] += str(res[1]) + ","
+
+
+totalemails = len(ms.keys())
+print("Start parsing mails ({} mails and {} paragraphs)".format(len(ms.keys()), len(ps.keys())))
+i, cnt, sum = 0, 0, 0
+db.flush()
+
 now = datetime.now()
+t = time.perf_counter()
+#idea here: per mail (m in ms) check its paragraphs (p in ms[m]) in which mails he occure, which arenot the current mail (tm in ps[p]). If we find a mail tm, which has the same amount of paragraphs, as the current mail does, it is a match mail, but the order of paragraphs is still ignored.
+#thus we check, if the current mail string occures in the match mail string. For all mails left, we delete the paragraph <-> mail connection.
+#for every mail...
+for i, m in enumerate(ms):
+    #find smallest paragraph and build list of mails
+    
+    tms = {}
+    matchmails = []
+    smallestP, small = -1, 1000
+    for p in ms[m]:
+        for tm in ps[p]:
+            if tm == m:
+                continue
+            if tm not in tms:
+                tms[tm] = 0
 
-for i, row in enumerate(results):
-    c = base.count(row[1])
-    if c > 1:
+            tms[tm] += 1
+            if tms[tm] == len(ms[m]):
+                matchmails.append(tm)
+
+    matchmails = [mail for mail in matchmails if mailParas[m] in mailParas[mail]]
+
+    if len(matchmails):
         cnt += 1
-        sum += c-1
-        mails.append(row[0])
+        sum += len(matchmails)
 
-    if not i%10000 and i > 0:
-        if mails != []:
-            db.execute("update mail_paragraphs set deleted = 1 where mailId in ({})"
-                           .format(','.join(np.array(mails).astype(str))), True)
-        mails = []
+        sql = "update mail_paragraphs set deleted = 1 where mailId in ({}) and pid in ({})".format(
+            ','.join(np.array(matchmails).astype(str)), ",".join(np.array(ms[m]).astype(str)))
+        try:
+            db.execute(sql)
+
+            for mail in matchmails:  # now delete this stuff in the local cache
+                for para in ms[m]:
+                    if mail in ps[para]:
+                        ps[para].remove(mail)
+                    mailParas[mail] = mailParas[mail].replace(str(para) + ",", "")
+
+                ms[mail] = [p for p in ms[mail] if p not in ms[m]]
+
+        except Exception as e:
+            print("{}: {}".format(type(e), str(e)))
+            print(sql)
+
+    if not i % 10000 and i > 0:
+        db.flush()
         dt = time.perf_counter() - t
-        etc = now + timedelta(seconds=(dt * totalemails / i))
+        etc = now + timedelta(seconds=(dt*(totalemails-i) / i))
 
-        print("{:>6} ({:>2}%) email checked, {:>6} redundant mails in {:>6} other mails found. Elapsed time: {:3.1f} min, ETC: {}"
-              .format(i, int(100*i/totalemails), cnt, sum, dt/60, etc.strftime('%H:%M:%S')))
+        print("{:>6} ({:>2}%) email checked, {:>6} redundant mails in {:>6} other mails found. "
+              "Elapsed time: {:3.1f} min, ETC: {}".format(i, int(100 * i / totalemails), cnt, sum, dt / 60, etc.strftime('%H:%M:%S')))
+db.flush()
 
-if mails != []:
-    db.execute("update mail_paragraphs set deleted = 1 where mailId in ({})"
-            .format(','.join(np.array(mails).astype(str))), True)
-
-print("{} redundant mails in {} other mails found. Elapsed time: {:3.1f} min".format(cnt, sum, dt/60))
+print("\nCleaning 'Original message' and 'Forwarded  by' paragraphs")
+db.execute("""
+update mail_paragraphs set deleted = 1
+where deleted = 0
+and sha in (
+  SELECT sha FROM sha_paragraphs
+  WHERE paragraph LIKE 'Original Message%'
+  OR paragraph LIKE 'Forwarded by % on %');""", commit=True)
